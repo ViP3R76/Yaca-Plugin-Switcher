a4d7cdbddbeffc546f91faa0cfb9eb8af3a44acb
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-08-28 19:28:57 +0200

    fix: disambiguate WPF localization and analyzer rule

--- src/YacaPluginSwitcher/GlobalUsings.cs
+++ src/YacaPluginSwitcher/GlobalUsings.cs
@@ -6,3 +6,4 @@ global using System.Windows.Controls;
 global using System.Windows.Input;
 global using System.Windows.Media;
 global using System.Windows.Media.Imaging;
+global using Localization = YacaPluginSwitcher.Configuration.Localization;
