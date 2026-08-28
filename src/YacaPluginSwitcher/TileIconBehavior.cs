7c3d5ca4a74937c0eebaa6ab3abeec0d1d350388
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-08-28 20:46:46 +0200

    fix: disambiguate WPF Path in tile icons

--- src/YacaPluginSwitcher/TileIconBehavior.cs
+++ src/YacaPluginSwitcher/TileIconBehavior.cs
@@ -1,7 +1,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Shapes;
+using Path = System.Windows.Shapes.Path;
+using Rectangle = System.Windows.Shapes.Rectangle;
 
 namespace YacaPluginSwitcher;
 
