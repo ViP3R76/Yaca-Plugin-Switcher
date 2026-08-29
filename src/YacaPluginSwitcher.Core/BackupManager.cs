83c434b38217183f5878c265c8ae99f177a47e47
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-08-29 12:56:43 +0200

    Persist YACA build in backup metadata

--- src/YacaPluginSwitcher.Core/BackupManager.cs
+++ src/YacaPluginSwitcher.Core/BackupManager.cs
@@ -43,6 +43,7 @@ public sealed class BackupManager
             fileInfo.Length,
             hash)
         {
+            SourceBuild = current?.Build,
             IsAutomatic = automatic
         };
 
