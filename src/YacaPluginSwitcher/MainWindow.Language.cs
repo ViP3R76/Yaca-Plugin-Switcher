94980f763042988dca435db56237b5eb61d330e4
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-08-29 20:10:24 +0200

    Sync navigation language and full-field selector

---
+++ src/YacaPluginSwitcher/MainWindow.Language.cs
@@ -0,0 +1,19 @@
+namespace YacaPluginSwitcher;
+
+public partial class MainWindow
+{
+    internal void RefreshNavigationLanguage()
+    {
+        BuildNavigation();
+        LoadLanguageSelector();
+        SetActiveNav(_activePage);
+    }
+
+    private void LanguageCombo_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (LanguageCombo.IsDropDownOpen) return;
+        LanguageCombo.Dispatcher.BeginInvoke(
+            System.Windows.Threading.DispatcherPriority.Input,
+            new Action(() => LanguageCombo.IsDropDownOpen = true));
+    }
+}
