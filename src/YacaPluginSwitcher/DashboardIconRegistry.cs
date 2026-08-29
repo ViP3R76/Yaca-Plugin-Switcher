2d7e955a74a9b096a68e2141e39f01463fd0c273
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-08-29 19:47:51 +0200

    Map TeamSpeak states to supplied SVG assets

--- src/YacaPluginSwitcher/DashboardIconRegistry.cs
+++ src/YacaPluginSwitcher/DashboardIconRegistry.cs
@@ -24,10 +24,9 @@ internal static class DashboardIconRegistry
     internal const string IconAssetSettings = "icon_asset_settings";
     internal const string IconAssetExit = "icon_asset_exit";
     internal const string IconAssetInstalled = "icon_asset_installed";
-    internal const string IconAssetTeamSpeak = "icon_asset_teamspeak";
-    internal const string IconAssetTeamSpeakActive = "icon_asset_teamspeak_active";
-    internal const string IconAssetTeamSpeakInactive = "icon_asset_teamspeak_inactive";
     internal const string IconAssetTeamSpeakStatus = "icon_asset_teamspeak_status";
+    internal const string IconAssetTeamSpeakStarted = "icon_asset_teamspeak_started";
+    internal const string IconAssetTeamSpeakStopped = "icon_asset_teamspeak_stopped";
     internal const string IconAssetSort = "icon_asset_sort";
 
     private static readonly Dictionary<string, string> AssetPaths = new(StringComparer.OrdinalIgnoreCase)
@@ -42,10 +41,9 @@ internal static class DashboardIconRegistry
         [IconAssetSettings] = "/Assets/settings-icon.svg",
         [IconAssetExit] = "/Assets/power-off-icon.svg",
         [IconAssetInstalled] = "/Assets/checked-shield-icon.svg",
-        [IconAssetTeamSpeak] = "/Assets/TS_InLine_Light.svg",
-        [IconAssetTeamSpeakActive] = "/Assets/TS_InLine_Light.svg",
-        [IconAssetTeamSpeakInactive] = "/Assets/power-off-icon.svg",
-        [IconAssetTeamSpeakStatus] = "/Assets/TS_InLine_Light.svg",
+        [IconAssetTeamSpeakStatus] = "/Assets/ts_stacked_light.svg",
+        [IconAssetTeamSpeakStarted] = "/Assets/checkmark-red-icon.svg",
+        [IconAssetTeamSpeakStopped] = "/Assets/checkmark-green-icon.svg",
         [IconAssetSort] = "/Assets/sort-toggle-icon.svg"
     };
 
