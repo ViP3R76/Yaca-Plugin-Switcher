482840b65234e104315f55a9a30c25406b9b921a
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-09-04 14:38:38 +0200

    Add shared System.Text namespace for updater formatting

--- src/YacaPluginSwitcher/GlobalUsings.cs
+++ src/YacaPluginSwitcher/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using System.Collections.ObjectModel;
 global using System.IO;
+global using System.Text;
 global using Microsoft.Win32;
 global using System.Windows;
 global using System.Windows.Controls;
