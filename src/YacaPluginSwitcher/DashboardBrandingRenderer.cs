eb7588bd49186ccdb79a5720cd2161196aa596ae
ViP3R_76 <45144344+ViP3R76@users.noreply.github.com>
2026-09-05 10:23:10 +0200

    Refine dashboard branding rendering

--- src/YacaPluginSwitcher/DashboardBrandingRenderer.cs
+++ src/YacaPluginSwitcher/DashboardBrandingRenderer.cs
@@ -9,17 +9,32 @@ public partial class MainWindow
 {
     private void BuildBrandingPanel(Grid host, int column)
     {
-        var card = CreatePanelCard((Brush)FindResource("AccentBrush"));
+        var card = new Border
+        {
+            Background = (Brush)FindResource("SurfaceBrush"),
+            BorderThickness = new Thickness(0),
+            Padding = new Thickness(20),
+            Margin = new Thickness(6)
+        };
+
         var branding = new Image
         {
-            Source = new BitmapImage(new Uri("/YacaPluginSwitcher;component/Assets/branding_logo.png", UriKind.RelativeOrAbsolute)),
-            Width = 230,
-            Height = 230,
+            Source = new BitmapImage(new Uri("/YacaPluginSwitcher;component/Assets/branding_logo.png", UriKind.RelativeOrAbsolute))
+            {
+                CacheOption = BitmapCacheOption.OnLoad
+            },
             Stretch = Stretch.Uniform,
             HorizontalAlignment = HorizontalAlignment.Center,
             VerticalAlignment = VerticalAlignment.Center,
+            MaxWidth = 520,
+            MaxHeight = DashboardPanelHeight - 40,
+            SnapsToDevicePixels = true,
             IsHitTestVisible = false
         };
+
+        RenderOptions.SetBitmapScalingMode(branding, BitmapScalingMode.HighQuality);
+        RenderOptions.SetEdgeMode(branding, EdgeMode.Aliased);
+
         card.Child = branding;
         Grid.SetColumn(card, column);
         host.Children.Add(card);
